cbd7d0c0717c14aea96bfd1a19a01fef85504059
galekkapi <kapigalson@gmail.com>
2026-01-14 22:07:53 +0100

    ustawienie mapy na srodku europy

--- static/index.js
+++ static/index.js
@@ -98,7 +98,7 @@ function startSimulation() {
 function goToStatistics() {
     window.location.href = '/category/statistics';
 }
-const map = L.map('map').setView([52.23, 21.01], 6);
+const map = L.map('map').setView([49.5, 15.5], 5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
